e5eaf76ea826ccd82ec3d05f28100e886369f759
Stefan Pochmann <609905+pochmann@users.noreply.github.com>
2026-08-10 20:15:45 +0200

    Simplify and speed up nth_permutation
    It was doing unnecessary computations and threw the results away.

--- more_itertools/more.py
+++ more_itertools/more.py
@@ -4305,11 +4305,10 @@ def nth_permutation(iterable, r, index):
         raise IndexError
 
     result = [0] * r
-    q = index * factorial(n) // c if r < n else index
-    for d in range(1, n + 1):
+    q = index
+    for d in range(n - r + 1, n + 1):
         q, i = divmod(q, d)
-        if 0 <= n - d < r:
-            result[n - d] = i
+        result[n - d] = i
         if q == 0:
             break
 
